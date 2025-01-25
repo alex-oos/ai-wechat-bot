27b3d9bd806a2f8b3e78c3c570f50860543d949b
Alex <46887064+alex-oos@users.noreply.github.com>
2025-01-25 19:37:17 +0800

    refactor(config): 新增 DeepSeekChatConfig 配置类
    - 在 com.wechat.bot.ai 包和 com.wechat.bot.config 包中分别新增了 DeepSeekChatConfig 类
    - 这两个类目前为空类，可能用于未来配置深寻聊天相关设置
    
    Signed-off-by: Alex <46887064+alex-oos@users.noreply.github.com>

---
+++ src/main/java/com/wechat.bot/config/DeepSeekChatConfig.java
@@ -0,0 +1,15 @@
+package com.wechat.bot.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+
+/**
+ * @author Alex
+ * @since 2025/1/25 19:35
+ * <p></p>
+ */
+@Configurable
+public class DeepSeekChatConfig {
+//    TODO
+
+
+}
