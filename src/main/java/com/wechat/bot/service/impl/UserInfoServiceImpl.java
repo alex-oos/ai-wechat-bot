7734202016c1fc1133b3a11addf70b0f12e859ce
Alex <46887064+alex-oos@users.noreply.github.com>
2025-03-24 17:47:16 +0800

    格式化

--- src/main/java/com/wechat/bot/service/impl/UserInfoServiceImpl.java
+++ src/main/java/com/wechat/bot/service/impl/UserInfoServiceImpl.java
@@ -1,8 +1,15 @@
 package com.wechat.bot.service.impl;
 
+import com.alibaba.fastjson2.JSONArray;
+import com.alibaba.fastjson2.JSONObject;
+import com.wechat.bot.entity.BotConfig;
+import com.wechat.bot.entity.ChatMessage;
 import com.wechat.bot.service.UserInfoService;
+import com.wechat.gewechat.service.ContactApi;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+import java.util.Collections;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -19,10 +26,54 @@ public class UserInfoServiceImpl implements UserInfoService {
      */
     private final Map<String, String> userInfo = new ConcurrentHashMap<>();
 
+    @Resource
+    private BotConfig botConfig;
+
     @Override
     public Map<String, String> getUserInfo() {
 
         return this.userInfo;
+    }
+
+    @Override
+    public void updateUserInfo(String userId) {
+
+        if (!userInfo.containsKey(userId)) {
+            // 存到一个map里面不用每次都重新获取，降低请求次数
+            // 获取好友的信息
+            String nickName = getNickname(userId);
+            if (nickName != null) {
+                userInfo.put(userId, nickName);
+            }
+        }
+
+
+    }
+
+    @Override
+    public void updateUserInfo(ChatMessage chatMessage) {
+        this.updateUserInfo(chatMessage.getFromUserId());
+        this.updateUserInfo(chatMessage.getToUserId());
+        chatMessage.setFromUserNickname(this.userInfo.get(chatMessage.getFromUserId()));
+        chatMessage.setToUserNickname(this.userInfo.get(chatMessage.getToUserId()));
+
+    }
+
+    private String getNickname(String userId) {
+
+        JSONObject briefInfo = ContactApi.getBriefInfo(botConfig.getAppId(), Collections.singletonList(userId));
+        if (briefInfo.getInteger("ret") == 200) {
+            JSONArray dataList = briefInfo.getJSONArray("data");
+            if (dataList.size() > 0) {
+                JSONObject userInfo = dataList.getJSONObject(0);
+                String remark = userInfo.getString("remark");
+                if (remark == null || remark.isBlank()) {
+                    return userInfo.getString("nickName");
+                }
+                return remark;
+            }
+        }
+        return null;
     }
 
 }
