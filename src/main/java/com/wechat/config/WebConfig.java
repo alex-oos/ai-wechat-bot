4acb60ce4903937b9621441ec0ffdd9dfa473129
Alex <46887064+alex-oos@users.noreply.github.com>
2025-03-26 14:11:21 +0800

    add:解决全局跨域

--- src/main/java/com/wechat/config/WebConfig.java
+++ src/main/java/com/wechat/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.wechat.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -28,6 +29,31 @@ public class WebConfig implements WebMvcConfigurer {
                 .addResourceLocations("file:" + IMAGE_PATH);
 
 
+    }
+
+    /**
+     * 解决全局跨域，
+     *
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 添加映射路径
+        registry.addMapping("/**")
+                // 放行哪些原始域
+                .allowedOriginPatterns("*")
+                // 是否发送Cookie信息
+                .allowCredentials(true)
+                // 放行哪些原始域(请求方式)
+                .allowedMethods("*")
+                // 放行哪些原始域(头部信息)
+                .allowedHeaders("*")
+                // 暴露哪些头部信息（因为跨域访问默认不能获取全部头部信息）
+                .exposedHeaders("access-control-allow-headers",
+                        "access-control-allow-methods",
+                        "access-control-allow-origin",
+                        "access-control-max-age",
+                        "X-Frame-Options");
     }
 
 }
