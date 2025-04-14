ba384f58b7d8d07d7c6cd12ab60f57d04dfef8e2
Alex <46887064+alex-oos@users.noreply.github.com>
2025-04-14 19:07:40 +0800

    ragflow:会话管理增加

---
+++ src/main/java/com/wechat/knowledgebase/ragflow/service/SessionService.java
@@ -0,0 +1,10 @@
+package com.wechat.knowledgebase.ragflow.service;
+
+/**
+ * @author Alex
+ * @since 2025/4/14 19:05
+ * <p></p>
+ */
+public interface SessionService {
+
+}
---
+++ src/main/java/com/wechat/knowledgebase/ragflow/service/impl/SessionServiceImpl.java
@@ -0,0 +1,15 @@
+package com.wechat.knowledgebase.ragflow.service.impl;
+
+
+import com.wechat.knowledgebase.ragflow.service.SessionService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Alex
+ * @since 2025/4/14 19:06
+ * <p></p>
+ */
+@Service
+public class SessionServiceImpl implements SessionService {
+
+}
