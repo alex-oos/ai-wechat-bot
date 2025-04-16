fb46c2f7f53efe96eec1848688a94de6f19e8228
Alex <46887064+alex-oos@users.noreply.github.com>
2025-04-16 23:08:21 +0800

    feat:dify 接口预留

---
+++ src/main/java/com/wechat/knowledgebase/dify/config/DifyConfig.java
@@ -0,0 +1,16 @@
+package com.wechat.knowledgebase.dify.config;
+
+/**
+ * @author Alex
+ * @since 2025/4/16 23:07
+ * <p></p>
+ */
+public class DifyConfig {
+
+    private String host;
+
+    private String apiKey;
+
+    private String rebootId;
+
+}
